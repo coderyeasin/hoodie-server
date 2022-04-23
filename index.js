8c26c2623d31a35e404449f4b93c6f29fe70d208
coderyeasin <yeasin3233@gmail.com>
2022-04-23 16:32:04 +0600

    stripe-api

--- index.js
+++ index.js
@@ -216,17 +216,17 @@ async function server() {
         //payment from user
 
         app.post("/create-payment-intent", async (req, res) => {
-            const paymentInfo = req.body;
-            const amount = paymentInfo.price * 100 //stripe uses cent--- 5 * 100
-            const paymentIntent = await stripe.paymentIntents.create({
-              amount: amount,
-              currency: "usd",
-              automatic_payment_methods: {
-                enabled: true,
-              },
-              //   payment_method_types:['card']
-            });
-            res.json({clientSecret: paymentIntent.client_secret})
+          const paymentInfo = req.body;
+          const amount = paymentInfo.price * 100; //stripe uses cent--- 5 * 100
+          const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: "usd",
+            automatic_payment_methods: {
+              enabled: true,
+            },
+            //   payment_method_types:['card']
+          });
+          res.json({ clientSecret: paymentIntent.client_secret });
         });
 
 
